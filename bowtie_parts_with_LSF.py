c8a46c03dd863696bcbb0182f3ef17be0153af66
Stuart Glenn <Stuart-Glenn@omrf.org>
2012-05-09 11:09:14 -0500

    Adds basic/quick ability to use SGE

--- bowtie_parts_with_LSF.py
+++ bowtie_parts_with_LSF.py
@@ -18,6 +18,19 @@ def submit_to_LSF(queue,LSFopfile,cmd_to_submit,mem_usage=None):
     #p.wait()
     return p.stdout.read().split('<')[1].split('>')[0]
 
+def submit_to_SGE(queue,log_file,cmd_to_submit,mem_usage=None):
+    # wrap command to submit in quotations
+    cmd_to_submit = r'"%s"' % cmd_to_submit.strip(r'"')
+    SGE_params = {'log_output':log_file,
+                      'queue':queue}
+    SGE_cmd = 'qsub -o %(log_output)s -b y -V -j y -cwd -q %(queue)s -N bowtie_parts' % SGE_params
+    if mem_usage != None:
+        SGE_cmd += r' -l h_vmem=%dG' % mem_usage
+    cmd = ' '.join([SGE_cmd,cmd_to_submit])
+    print cmd
+    p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
+    #p.wait()
+    return p.stdout.read()
 
 argparser = argparse.ArgumentParser(description=None)
 argparser.add_argument('-i','--input',required=True)
@@ -47,4 +60,4 @@ for infilename in glob.glob(os.path.join(input_dir,'*.fastq')):
     params['reads'] = infilename
     params['alignments'] = outfilename
     
-    print submit_to_LSF(args.queue,logfilename,bowtie_cmd % params)
+    print submit_to_SGE(args.queue,logfilename,bowtie_cmd % params,4)
